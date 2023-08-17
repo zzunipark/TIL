db2d2cfe0eb0a959fc6377363dd71ce52cf6f722
MinJun Park <gusoong0829@naver.com>
2023-08-17 18:13:36 +0900

    TIL

---
+++ &mpersand-homework/Vanilla-JS_ChromeApp/app.js
@@ -0,0 +1,9 @@
+const player = {
+    name: "minjun",
+    sayHello: function(otherPersonName) {
+        console.log("Hello, " + otherPersonName + ". Nice to meet you!");
+    },
+};
+
+console.log(player.name);
+player.sayHello("Park");
