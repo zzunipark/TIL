6ffc1fb2f4ac0481877e8a025db1fdbfa314cb0f
MinJun Park <gusoong0829@naver.com>
2023-08-24 21:12:34 +0900

    TIL

--- &mpersand-homework/Vanilla-JS_ChromeApp/app.js
+++ &mpersand-homework/Vanilla-JS_ChromeApp/app.js
@@ -1,8 +1,28 @@
-const age = 96;
-function calculateKrAge(ageOfForeigner) {
-    return ageOfForeigner + 2;
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem("USERNAME_KEY", username);
+    paintGreetings(username);
 }
 
-const krAge = calculateKrAge(age);
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
-console.log(krAge)
+const savedUsername = localStorage.getItem("USERNAME_KEY");
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
